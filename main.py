2e0f24f39baaaec8a74a6e22819921c373ed1178
Nimira43 <n.rabone72@aol.com>
2025-02-01 09:48:27 +0000

    Importing libraries

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print('Hello')
+import numpy as np
+import pandas as pd
+from textblob import TextBlob
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.pipeline import make_pipeline
